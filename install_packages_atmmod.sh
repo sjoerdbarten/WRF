24068edd937fc641fa4b57ffebc64431f9cda40b
Sjoerd Barten <156204145+sjoerdbarten@users.noreply.github.com>
2024-06-11 11:01:20 +0200

    Add files via upload

---
+++ install_packages_atmmod.sh
@@ -0,0 +1,10 @@
+#module unload intel/compiler/64/2017
+/shared/legacyapps/python/3.8.5/bin/python -m pip install --no-cache-dir --user --upgrade pip
+python -m pip install --no-cache-dir --user numexpr==2.7.3
+python -m pip install --no-cache-dir --user numpy==1.20.3
+python -m pip install --no-cache-dir --user python-dateutil==2.8.2
+python -m pip install --no-cache-dir --user basemap==1.3.9
+python -m pip install --no-cache-dir --user pandas==2.0.3
+python -m pip install --no-cache-dir --user shapely==1.8.5 --no-binary shapely --ignore-installed
+python -m pip install --no-cache-dir --user numpy==1.23.1
+python -m pip install --no-cache-dir --user wrf-python
