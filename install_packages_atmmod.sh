d566204a7cea0f4019abef570466741c9bd2d8b4
Sjoerd Barten <sjoerd.barten@live.nl>
2025-02-10 15:57:25 +0100

    Update install_packages_atmmod.sh

--- install_packages_atmmod.sh
+++ install_packages_atmmod.sh
@@ -1,10 +1,14 @@
-#module unload intel/compiler/64/2017
-/shared/legacyapps/python/3.8.5/bin/python -m pip install --no-cache-dir --user --upgrade pip
-python -m pip install --no-cache-dir --user numexpr==2.7.3
-python -m pip install --no-cache-dir --user numpy==1.20.3
-python -m pip install --no-cache-dir --user python-dateutil==2.8.2
-python -m pip install --no-cache-dir --user basemap==1.3.9
-python -m pip install --no-cache-dir --user pandas==2.0.3
-python -m pip install --no-cache-dir --user shapely==1.8.5 --no-binary shapely --ignore-installed
-python -m pip install --no-cache-dir --user numpy==1.23.1
-python -m pip install --no-cache-dir --user wrf-python
+PYTHONWARNINGS="ignore"
+rm -r .local
+python -m pip install --no-cache-dir --user --upgrade pip  2>/dev/null
+python -m pip install --no-cache-dir --user numexpr==2.7.3  2>/dev/null
+python -m pip install --no-cache-dir --user numpy==1.20.3  2>/dev/null
+python -m pip install --no-cache-dir --user python-dateutil==2.8.2  2>/dev/null
+python -m pip install --no-cache-dir --user basemap==1.3.9  2>/dev/null
+python -m pip install --no-cache-dir --user pandas==2.0.3  2>/dev/null
+python -m pip install --no-cache-dir --user shapely==1.8.5 --no-binary shapely --ignore-installed  2>/dev/null
+python -m pip install --no-cache-dir --user numpy==1.23.1  2>/dev/null
+python -m pip install --no-cache-dir --user wrf-python  2>/dev/null
+python -m pip install --no-cache-dir --user metpy  2>/dev/null
+#setfacl -R -m d:u:barte035:rwx .
+#setfacl -R -m u:barte035:rwx .
