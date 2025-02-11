a2c818d33e0386dd7dd5c5055a323b91f78ed884
Sjoerd Barten <sjoerd.barten@live.nl>
2025-02-11 16:56:51 +0100

    Add files via upload

--- install_packages_atmmod.sh
+++ install_packages_atmmod.sh
@@ -1,14 +1,12 @@
 PYTHONWARNINGS="ignore"
 rm -r .local
 python -m pip install --no-cache-dir --user --upgrade pip  2>/dev/null
-python -m pip install --no-cache-dir --user numexpr==2.7.3  2>/dev/null
-python -m pip install --no-cache-dir --user numpy==1.20.3  2>/dev/null
 python -m pip install --no-cache-dir --user python-dateutil==2.8.2  2>/dev/null
 python -m pip install --no-cache-dir --user basemap==1.3.9  2>/dev/null
-python -m pip install --no-cache-dir --user pandas==2.0.3  2>/dev/null
 python -m pip install --no-cache-dir --user shapely==1.8.5 --no-binary shapely --ignore-installed  2>/dev/null
-python -m pip install --no-cache-dir --user numpy==1.23.1  2>/dev/null
 python -m pip install --no-cache-dir --user wrf-python  2>/dev/null
 python -m pip install --no-cache-dir --user metpy  2>/dev/null
+python -m pip install --no-cache-dir --user cartopy  2>/dev/null
+python -m pip install --no-cache-dir --user netCDF4  2>/dev/null
 #setfacl -R -m d:u:barte035:rwx .
 #setfacl -R -m u:barte035:rwx .
